b90f8f52ec6af2aa5529f2a9529af676d7fbdfde
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2024-08-08 01:24:29 +0200

    Update to use TextStoring

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
         .library(name: "TextFormationPlugin", targets: ["TextFormationPlugin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/STTextView", from: "0.8.13"),
-        .package(url: "https://github.com/ChimeHQ/TextFormation", from: "0.8.1"),
-		.package(url: "https://github.com/ChimeHQ/TextStory", from: "0.8.0"),
+        .package(url: "https://github.com/krzyzanowskim/STTextView", from: "0.9.6"),
+        .package(url: "https://github.com/ChimeHQ/TextFormation", from: "0.9.0"),
+		.package(url: "https://github.com/ChimeHQ/TextStory", from: "0.9.0"),
     ],
     targets: [
         .target(
